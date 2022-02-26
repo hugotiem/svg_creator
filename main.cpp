6edbb5bad21d50e5fbf039c4620046ad603eecdb
julienduponchel <duponchel.julien@outlook.fr>
2022-02-26 15:55:00 +0100

    ajout de graphics.h et graphics.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include "src/Rectangle.cpp"
 #include "src/include/Circle.h"
 
@@ -38,4 +38,33 @@ int main() {
     }
 
     return 0;
+}*/
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <iostream>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "Graphics.h"
+
+void drawScene(void) {
+    clearWindow();
+    setColor(YELLOW);
+    drawFilledTriangle(200,125,100,375,200,375);
+    setColor(BLACK);
+    drawLine(200,200,400,400);
+    setColor(RED);
+    drawFilledCircle(100,100,100);
+    setColor(MAGENTA);
+    drawFilledBox(300,300,400,400);
+    glEnd();
+    glutSwapBuffers();
+}
+
+int main(int argc, char ** argv) {
+    graphicsSetup(argc, argv);
+    glutDisplayFunc(drawScene);
+    glutMainLoop();
 }
