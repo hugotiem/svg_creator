1cfcb442b9ae6614112251469fc36e9bda9adc1a
julienduponchel <duponchel.julien@outlook.fr>
2022-02-26 16:17:20 +0100

    fix: file not found error

---
+++ cmake-build-debug/CMakeFiles/svg_creator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/svg_creator.dir/Graphics.cpp.o"
+  "CMakeFiles/svg_creator.dir/Graphics.cpp.o.d"
+  "CMakeFiles/svg_creator.dir/main.cpp.o"
+  "CMakeFiles/svg_creator.dir/main.cpp.o.d"
+  "svg_creator"
+  "svg_creator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/svg_creator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
